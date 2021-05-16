d684127234dcc0e2e1990f7216f729d782677098
Peterson <petersonsimiao1@gmail.com>
2021-05-16 02:22:30 -0300

    docs(storybook): inicializado storybook

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: [
+    "../components/**/stories.@(js|jsx|ts|tsx)",
+    "../pages/**/stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
