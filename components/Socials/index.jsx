f11aea623188eb08dfb6aa2f12d5255967f54eb4
Peterson <petersonsimiao1@gmail.com>
2021-11-08 20:54:50 -0300

    fix(Socials Componente): Adicionado prop key na lista de redes sociais

--- components/Socials/index.jsx
+++ components/Socials/index.jsx
@@ -4,8 +4,8 @@ import { socials } from "./models";
 function Socials() {
   return (
     <List>
-      {socials.map(({ link, alt, src, invert }) => (
-        <Item invert={invert}>
+      {socials.map(({ link, alt, src, invert }, i) => (
+        <Item key={i} invert={invert}>
           <a href={link} target="_blank" rel="noreferrer">
             <img height="34px" width="34px" src={src} alt={alt} />
           </a>
